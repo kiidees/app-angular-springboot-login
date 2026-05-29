54115218879429cc547e7ca7de3e5bd488a236c0
kiidees <kiidees@gmail.com>
2026-05-28 19:01:54 -0600

    auth with gcp

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,18 +3,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-/**** 
- * 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 
- * 762559686416-jm4df29i36p9g0boj5d225khdhp0bqj1.apps.googleusercontent.com
- * ****/
 
-
-/****
- * 
- * 762559686416-2had8klsm955lmnboc8eioa52ul6ke5e.apps.googleusercontent.com
- * 
- * 
- */
 
 import { routes } from './app/app.routes';      
 
